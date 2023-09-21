74a57df9343103dd6e1e9e018ddc7c864ed9c204
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-09-21 15:01:27 -0300

    Adição de cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const rotas = require('./rotas');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(rotas);
 
