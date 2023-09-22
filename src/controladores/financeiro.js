63bca9219465a077de4297e972178d6efe172180
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-09-22 19:36:01 -0300

    Aprimoramento do cadastro

--- src/controladores/financeiro.js
+++ src/controladores/financeiro.js
@@ -1,18 +1,60 @@
 const knex = require("../conexao");
+const { dataTratada } = require("../uteis/data");
 
 const listagemParametrosTabela = async (req, res) => {
   try {
     const tipos = await knex("categorias");
     const sub_categorias = await knex("sub_categorias");
 
-    const subtipos = sub_categorias.sort((a, b) => a.descricao.localeCompare(b.descricao));
+    const subtipos = sub_categorias.sort((a, b) =>
+      a.descricao.localeCompare(b.descricao)
+    );
 
-    res.status(200).json({ tipos, subtipos});
+    res.status(200).json({ tipos, subtipos });
   } catch (error) {
     res.status(500).json({ mensagem: "Erro no servidor." });
   }
 };
 
+const cadastroFinanceiro = async (req, res) => {
+  const { data, descricao, tipo, subtipo, valor } = req.body;
+
+  const cadastroFinanceiro = {
+    data,
+    descricao,
+    categoria_id: tipo,
+    valor,
+    usuario_id: req.usuario.id,
+  };
+
+  try {
+    const [financeiro] = await knex("financeiro")
+    .insert(cadastroFinanceiro)
+    .returning("*");
+
+    const categorias = await knex("financeiro")
+      .join("categorias", "financeiro.categoria_id", "categorias.id")
+      .where("categorias.id", tipo)
+      .select("categorias.descricao as categoria")
+      .first();
+
+      const sub_categorias = await knex("financeiro")
+      .join("sub_categorias", "financeiro.categoria_id", "sub_categorias.categoria_id")
+      .where("sub_categorias.id", subtipo)
+      .select("sub_categorias.descricao as sub_categoria")
+      .first();
+
+      financeiro.tipo = categorias.categoria;
+      financeiro.subtipo = sub_categorias.sub_categoria;
+  
+  res.status(201).json(financeiro);
+  
+  } catch (error) {
+    res.status(500).json({ mensagem: "Erro no servidor" });
+  }
+};
+
 module.exports = {
   listagemParametrosTabela,
+  cadastroFinanceiro,
 };
--- src/rotas.js
+++ src/rotas.js
@@ -2,17 +2,17 @@ const express = require('express');
 const { cadastrarUsuario, login } = require('./controladores/usuarios');
 const verificaLogin = require('./intermediarios/verificarLogin');
 const { validarDados, validarEmailExistente, validarTransacao } = require('./intermediarios/validacoes');
-const { listagemParametrosTabela } = require('./controladores/financeiro');
+const { listagemParametrosTabela, cadastroFinanceiro } = require('./controladores/financeiro');
 
 const rotas = express();
 
 rotas.post("/cadastrar", validarDados, validarEmailExistente,cadastrarUsuario);
 rotas.post("/login", login);
 
-rotas.get('/listagemParametrosTabela', listagemParametrosTabela);
+rotas.get('/listagemparametrostabela', listagemParametrosTabela);
 
 rotas.use(verificaLogin);
 
-// rotas.get('/')
+rotas.post('/cadastrofinanceiro', cadastroFinanceiro);
 
 module.exports = rotas;
