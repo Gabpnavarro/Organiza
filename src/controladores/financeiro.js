01afac91a9d1465ae8410d03e911628f2a89507c
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-09-22 13:14:18 -0300

    Ordenando listagem de subtipos

--- src/controladores/financeiro.js
+++ src/controladores/financeiro.js
@@ -3,8 +3,11 @@ const knex = require("../conexao");
 const listagemParametrosTabela = async (req, res) => {
   try {
     const tipos = await knex("categorias");
-    const subtipos = await knex("sub_categorias");
-    res.status(200).json({ tipos, subtipos });
+    const sub_categorias = await knex("sub_categorias");
+
+    const subtipos = sub_categorias.sort((a, b) => a.descricao.localeCompare(b.descricao));
+
+    res.status(200).json({ tipos, subtipos});
   } catch (error) {
     res.status(500).json({ mensagem: "Erro no servidor." });
   }
