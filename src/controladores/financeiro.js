154f9493a17a5c588a10853180f2de5fec376aa3
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-09-22 20:39:35 -0300

    Listagem de tipo e subtipo alterado

--- src/controladores/financeiro.js
+++ src/controladores/financeiro.js
@@ -3,14 +3,20 @@ const { dataTratada } = require("../uteis/data");
 
 const listagemParametrosTabela = async (req, res) => {
   try {
-    const tipos = await knex("categorias");
-    const sub_categorias = await knex("sub_categorias");
 
-    const subtipos = sub_categorias.sort((a, b) =>
-      a.descricao.localeCompare(b.descricao)
+
+    const tipoEsubtipos = await knex("categorias")
+    .select("categorias.id as tipo_id", "categorias.descricao as tipo_descricao")
+    .leftJoin("sub_categorias", "categorias.id", "sub_categorias.categoria_id")
+    .select("sub_categorias.id as subtipo_id", "sub_categorias.descricao as subtipo_descricao")
+    .orderBy("categorias.descricao")
+    .orderBy("sub_categorias.descricao");
+
+    const tipoEsubtiposOrdenados = tipoEsubtipos.sort((a, b) =>
+      a.subtipo_descricao.localeCompare(b.subtipo_descricao)
     );
 
-    res.status(200).json({ tipos, subtipos });
+    res.status(200).json(tipoEsubtiposOrdenados);
   } catch (error) {
     res.status(500).json({ mensagem: "Erro no servidor." });
   }
@@ -30,7 +36,7 @@ const cadastroFinanceiro = async (req, res) => {
   try {
     const [financeiro] = await knex("financeiro")
     .insert(cadastroFinanceiro)
-    .returning("*");
+    .returning(["descricao","valor","data"]);
 
     const categorias = await knex("financeiro")
       .join("categorias", "financeiro.categoria_id", "categorias.id")
@@ -44,6 +50,7 @@ const cadastroFinanceiro = async (req, res) => {
       .select("sub_categorias.descricao as sub_categoria")
       .first();
 
+      financeiro.data = dataTratada(data);
       financeiro.tipo = categorias.categoria;
       financeiro.subtipo = sub_categorias.sub_categoria;
   
--- src/uteis/data.js
+++ src/uteis/data.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 const dataTratada = (data) => {
 
-    const date = moment(data).format('DD/MM/YYYY');
+    const date = moment(data, "DD/MM/YYYY").format("DD/MM/YYYY");
     
     return date;
 }
