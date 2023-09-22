827844c2f92bebd9420e9a17687ad0b5b966e038
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-09-22 12:35:47 -0300

    Correção do cors

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// const allowedOrigins = ['http://localhost:3000', 'https://defiant-seal-wetsuit.cyclic.app', 'https://gerenciamento-financas.cyclic.app' ];
-// {
-//   origin: allowedOrigins,
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   headers: 'Origin, X-Requested-With, Content-Type, Accept'
-// }
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000'];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  headers: 'Origin, X-Requested-With, Content-Type, Accept'
+}));
 
 
 app.use(express.json());
