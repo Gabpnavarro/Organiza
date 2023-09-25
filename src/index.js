3de815db9d17fc373906490a6d08cb30ed37b716
Gabriel Navarro <gabpnavarro@hotmail.com>
2023-09-25 12:25:29 -0300

    Cors - alteração

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000'];
+// const allowedOrigins = ['http://localhost:3000'];
 app.use(cors({
-  origin: allowedOrigins,
+  // origin: allowedOrigins,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   headers: 'Origin, X-Requested-With, Content-Type, Accept'
 }));
